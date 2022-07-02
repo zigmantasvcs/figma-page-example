0ce8afe3db292aa0d234e35bb0c17ee18a630290
Zigmantas <zigmantas@mrorangetech.se>
2022-07-02 18:50:30 +0300

    modal functionality added

---
+++ index.js
@@ -0,0 +1,20 @@
+document.querySelectorAll(".artist-block img, .gallery-block img").forEach(function(element){
+    element.onclick = function(){
+        document.getElementById("modal-image").src = element.src;
+        document.querySelector(".modal").style.display = "flex";
+        document.querySelector(".modal").classList.add("fadein");
+
+    }    
+})
+
+document.querySelector(".modal").onclick = function(){
+    document.querySelector(".modal").classList.remove("fadein");
+    document.querySelector(".modal").classList.add("fadeout");
+}
+
+document.querySelector(".modal").addEventListener("animationend", (ev) => {
+    if (document.querySelector(".modal").classList.contains("fadeout")) {
+        document.querySelector(".modal").style.display = "none";
+        document.querySelector(".modal").classList.remove("fadeout");
+    }
+  });
